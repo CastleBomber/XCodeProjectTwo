b18f5696f273be13c95621d94497400fcc417cdb
cBombs <richie.ucsb@gmail.com>
2020-01-06 19:12:12 -0800

    sitting in the meeting, hehe

---
+++ XCodeProjectTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCodeProjectTwo
+//
+//  Created by Richie on 12/26/19.
+//  Copyright © 2019 CastleBomber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
